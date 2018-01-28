8a6c70c5c35cc01987db3a94a21984b375cbf0d4
Matthew Beatty <beattyml1@gmail.com>
2018-01-27 23:42:05 -0500

    Get some basic redux state management code started

--- metatonic-redux/src/redux-state-manager.ts
+++ metatonic-redux/src/redux-state-manager.ts
@@ -1,1 +1,17 @@
+import { createStore } from 'redux';
+import {
+    FormState, FormStateChanges, FormEvent, StateEvents
+} from "metatonic-core/src/index";
 
+export function startStateManager() {
+    createStore((state: FormState, action: FormEvent) => {
+        let formStateChanges = new FormStateChanges();
+        switch (action.event) {
+            case StateEvents.formServerUpdate: return formStateChanges.formServerUpdate(state, action.data);
+            case StateEvents.propertiesChanged: return formStateChanges.propertiesChanged(state, action.data);
+            case StateEvents.propertyChanged: return formStateChanges.propertyChanged(state, action.data);
+            case StateEvents.fullReload: return formStateChanges.fullReload(state, action.data);
+            case StateEvents.trySubmit: return formStateChanges.trySubmit(state, action.data);
+        }
+    });
+}

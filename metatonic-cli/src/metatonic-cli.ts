85b9bdf8486370d7792b4dc5bad6c3cac50ae886
Matthew Beatty <beattyml1@gmail.com>
2018-01-28 16:39:57 -0500

    Started cli

---
+++ metatonic-cli/src/metatonic-cli.ts
@@ -0,0 +1,56 @@
+import {Rest} from "../../metatonic-core/src/services/Rest";
+
+export function doCli(action: string, subject, ...args) {
+    switch (action) {
+        case "add": add(...args);
+        case "init": init(...args);
+        case "test": test(...args);
+        case "run": run(...args);
+    }
+}
+
+export function add(type: string, name: string, type?: string, ...args) {
+    switch (type) {
+        case "model": addModel(name);
+        case "form": addForm(name, type);
+        case "editor": addEditor(name, type);
+        case "multiEdit": addMultiEdit(name, type);
+        case "select": addSelect(name, type);
+    }
+}
+
+export function init(name: string, schemaSource: string) {
+    if (/https?:\/\/.*/g.test(schemaSource)) {
+        var schemaSourceType = 'json-remote'
+    } else {
+        var schemaSourceType = /.*\.json/g.test(schemaSource) ? 'json-local' : /.*\.ts/g.test(schemaSource) ? 'file-local' : 'local';
+    }
+}
+
+export function run() {
+
+}
+
+export function test() {
+
+}
+
+export function addModel(name) {
+
+}
+
+export function addForm(name, type) {
+
+}
+
+export function addEditor(name, type) {
+
+}
+
+export function addMultiEdit(name, type) {
+
+}
+
+export function addSelect(name, type) {
+
+}

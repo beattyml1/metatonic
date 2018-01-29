c8a294f3b634d868767619ec01b0c319cbf09626
Matthew Beatty <beattyml1@gmail.com>
2018-01-28 20:19:17 -0500

    Started routing code

---
+++ metatonic-router/src/MetatonicRouter.ts
@@ -0,0 +1,22 @@
+import Navigo from 'navigo';
+import {FormSchema} from "metatonic-core/src/index";
+
+export class MetatonicRouter {
+    constructor() {
+        var root = null;
+        var useHash = true; // Defaults to: false
+        var hash = '#!'; // Defaults to: '#'
+        var router = new Navigo(root, useHash, hash);
+        router
+            .on('/f/:formName', (params: { formName }) => { })
+            .on('/f/:formName/:recordId', (params: { formName, recordId} ) => { })
+            .on('/r/:recordType/:recordId', (params: { recordType, recordId} ) => { })
+            .on('/r/:recordType/:recordId/display', (params: { recordType, recordId} ) => { })
+            .on('/r/:recordType/list/:groupName', (params: { recordType, groupName}, query ) => { })
+            .on('/r/:recordType/', (params: { recordType}, query ) => { })
+    }
+
+    displayForm(name: string, schema: FormSchema) {
+
+    }
+}

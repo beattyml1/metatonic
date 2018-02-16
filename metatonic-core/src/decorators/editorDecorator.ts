ba346759a34dd941bcb12d461ddeabdbe7c04459
Matthew Beatty <beattyml1@gmail.com>
2018-02-16 16:58:27 -0500

    all the user to send in a class and get the name of the type off of that so that autocomplete and strong typing is a thing

--- metatonic-core/src/decorators/editorDecorator.ts
+++ metatonic-core/src/decorators/editorDecorator.ts
@@ -1,9 +1,19 @@
 import {editorRegistry, EditorRegistry, multiEditRegistry, selectRegistry} from "../services/EditorRegistry";
 
 function editorRegistrationDecorator(registry: EditorRegistry<any, any, any>) {
-    return function(type: string, labeler: new (...args) => any, options?: { uiHint?: string|string[], isDefault?: boolean, repeater?: new (...args) => any }) {
+    return function(
+        type: string| (new (...args) => any),
+        labeler: new (...args) => any,
+        options?: {
+            uiHint?: string|string[],
+            isDefault?: boolean,
+            repeater?: new (...args) => any
+        }) {
+        let typeStr = typeof type === "string" ? type :
+                      typeof type === "function" ? type.name : "";
+
         return function(constructor: Function) {
-            editorRegistry.registerComponent(type, constructor, labeler, options);
+            editorRegistry.registerComponent(typeStr, constructor, labeler, options);
         }
     }
 }
